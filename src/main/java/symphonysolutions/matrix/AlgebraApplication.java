1053502f44e19b5f020f10e95e220602570da167
RostyMac <avenger2010ua@gmail.com>
2019-05-15 21:17:35 +0300

    init

---
+++ src/main/java/symphonysolutions/matrix/AlgebraApplication.java
@@ -0,0 +1,13 @@
+package symphonysolutions.matrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgebraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgebraApplication.class, args);
+	}
+
+}
